c20858fa277f89eb8678319c9e9c11364c529de4
Faysal Mridha <fmridha166@gmail.com>
2026-04-29 23:06:53 +0600

    feat: add staff module with routes, controller, service, and validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,26 +2,31 @@ import { Router } from "express";
 import authRoutes from "../modules/Auth/auth.route";
 import { userRoutes } from "../modules/User/user.routes";
 import { adminRoutes } from "../modules/Admin/admin.routes";
+import { staffRoutes } from "../modules/Staff/staff.routes";
 
 const router = Router();
 
 const routes: { path: string; route: Router }[] = [
-    {
-        path: "/auth",
-        route: authRoutes,
-    },
-    {
-        path: "/user",
-        route: userRoutes,
-    },
-    {
-        path: "/admin",
-        route: adminRoutes,
-    },
+  {
+    path: "/auth",
+    route: authRoutes,
+  },
+  {
+    path: "/user",
+    route: userRoutes,
+  },
+  {
+    path: "/admin",
+    route: adminRoutes,
+  },
+  {
+    path: "/staff",
+    route: staffRoutes,
+  },
 ];
 
 routes.forEach((route) => {
-    router.use(route.path, route.route);
+  router.use(route.path, route.route);
 });
 
 export default router;
