68e553dd71c865543685dd3c98e4e1311e935f00
meshal10613 <syedmohiuddinmeshal24@gmail.com>
2026-06-11 14:32:47 +0600

    feat: small changes applied

--- src/modules/Auth/auth.validation.ts
+++ src/modules/Auth/auth.validation.ts
@@ -71,20 +71,25 @@ const resetPasswordValidation = z
     })
     .strict();
 
-const changePasswordValidation = z.object({
-        password: z
+const changePasswordValidation = z
+    .object({
+        currentPassword: z
             .string()
             .min(8, "Password must be at least 8 characters")
             .max(100),
-    newPassword: z
-        .string()
-        .min(8, "Password must be at least 8 characters")
-        .max(100)
-        .regex(/[A-Z]/, "Must include at least one uppercase letter")
-        .regex(/[a-z]/, "Must include at least one lowercase letter")
-        .regex(/[0-9]/, "Must include at least one number")
-        .regex(/[^A-Za-z0-9]/, "Must include at least one special character"),
-}).strict();
+        newPassword: z
+            .string()
+            .min(8, "Password must be at least 8 characters")
+            .max(100)
+            .regex(/[A-Z]/, "Must include at least one uppercase letter")
+            .regex(/[a-z]/, "Must include at least one lowercase letter")
+            .regex(/[0-9]/, "Must include at least one number")
+            .regex(
+                /[^A-Za-z0-9]/,
+                "Must include at least one special character",
+            ),
+    })
+    .strict();
 
 const authValidator = {
     registerValidation,
